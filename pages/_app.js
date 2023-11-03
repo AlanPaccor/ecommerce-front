9f7c05e9f53921e9ce1434c74735c11dbbb63538
Alan Paccor <alan@Alans-Air.lan1>
2023-11-03 03:18:16 -0400

    started on basic header and styles

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyles = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Poppins&family=Roboto&family=Ysabeau+Infant&display=swap');
+body{
+  padding:0;
+  margin:0;
+  font-family: 'Poppins', sans-serif;
+}
+`;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+    <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
+}
