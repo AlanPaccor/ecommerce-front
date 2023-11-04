a2cb6d964a6f32fe98f0e5fa8221bf2a977d3bd2
Alan Paccor <alan@Alans-MacBook-Air.local>
2023-11-04 04:54:40 -0400

    done with product boxes and new arrivals, next is add to cart function

--- components/NewProducts.js
+++ components/NewProducts.js
@@ -1,17 +1,25 @@
 import styled from "styled-components";
 import Center from "./Center";
+import ProductBox from "./ProductBox";
 
 const ProductsGrid = styled.div`
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 20px;
+`;
+const Title = styled.div`
+    font-size: 2rem;
+    margin: 30px 0 20px;
+    font-weight: normal;
 `;
 
 export default function NewProducts({products}) {
     return (
         <Center>
+            <Title>New Arrivals</Title>
             <ProductsGrid>
                 {products?.length > 0 && products.map(product => (
-                    <div>{product.title}</div>
+                    <ProductBox {...product} />
                 ))}
             </ProductsGrid>
         </Center>
